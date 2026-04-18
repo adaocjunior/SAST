a0d91779e008e12546c991402165c505c8455576
Adão Júnior <adao8678@gmail.com>
2026-04-17 22:04:00 -0300

    Efetuada sanitizacao de Busca

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from flask import Flask, request
 
 APP = Flask(__name__)
 
+def escape(texto: str) -> str:
+    texto = texto.replace("<", "&alt;")
+    texto = texto.replace("<", "&gt;")
+    return texto
+
 @APP.route('/busca')
 def query():
     busca = request.args.get('q', '')
-
-    return f"<h2>Resultados para: {busca}</h2>"
+    busca_sanitizada = escape(busca)
+    return f"<h2>Resultados para: {busca_sanitizada}</h2>"
 
 if __name__ == '__main__':
     APP.run(port=5000)
