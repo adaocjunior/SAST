75dcc15dedc1118d787b9e6216880c51c3f23ff1
Adão Júnior <adao8678@gmail.com>
2026-04-17 21:00:56 -0300

    Correcao - Codigo

--- calc.py
+++ calc.py
@@ -1,12 +1,13 @@
 def main ():
     soma = 0
     try:
-        numeros = eval(input("Envie uma lista separada por virgula de numeros"))
-    except SyntaxError:
+        numeros = (input("Envie uma lista separada por virgula de numeros")).split(",")
+        for num in numeros:
+            soma = soma + int(num)
+        print(f"Soma de {numeros} = {soma}")
+    except ValueError:
         print("Input invalido")
-    for num in numeros:
-        soma = soma + num
-    print(f"Soma de {numeros} = {soma}")
+        return
 
 if __name__ == '__main__()':
     main()
